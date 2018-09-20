f6dd8cba0bc6f33fbf162a948dfb485f6d1e0ed7
Aaron Chavez <achavez@corporaciondpt.com>
2018-09-19 19:24:15 -0600

    finished lesson 13

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -8,6 +8,12 @@ import { NgForm } from '@angular/forms';
   .ng-invalid.ng-touched:not(form) {
     border: 1px solid red;
   }
+  .invalid{
+    font-family: "Times New Roman";
+    font-size: large;
+    font-style: italic;
+    color: red;
+  }
   `]
 })
 export class TemplateComponent implements OnInit {
@@ -15,8 +21,26 @@ export class TemplateComponent implements OnInit {
   user = {
     name: null,
     lastName: null,
-    email: null
+    email: null,
+    country: "",
+    sex: "Male",
+    accept: false
   };
+
+  sexs = ["Male","Female"];
+
+  countries = [{
+    code: 'CR',
+    country: 'Costa Rica'
+  },
+  {
+    code: 'PAN',
+    country: 'Panama'
+  },
+  {
+    code: 'PORT',
+    country: 'Portugal'
+  }]
 
   constructor() { }
 
