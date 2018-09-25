ca3b6063131b148d5375955131614329cbfa7de5
Aaron Chavez <achavez@corporaciondpt.com>
2018-09-24 19:00:07 -0600

    finished lesson 18

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,13 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
-  styles: []
+  styles: [`
+  .ng-invalid.ng-touched:not(form) {
+    border: 1px solid red;
+  }
+  .invalid{
+    font-family: "Times New Roman";
+    font-size: large;
+    font-style: italic;
+    color: red;
+  }
+  `]
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+  user = {
+    fullName: {
+      name: 'Henry',
+      lastName: 'Chaves'
+    },
+    email: 'some@some.com'
+  };
+
+  constructor() { 
+
+    console.log(this.user);
+
+    this.forma = new FormGroup({
+
+      'fullName': new FormGroup({
+        'name': new FormControl('', [Validators.required, Validators.minLength(3)] ),
+        'lastName': new FormControl('', Validators.required)
+      }),
+      'email': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+    })
+
+
+
+  }
+
+
+  saveChanges(){
+    console.log(this.forma.value);
+  }
 
   ngOnInit() {
   }
