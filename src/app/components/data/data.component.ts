aaf8c0473074a18d06d21b6f92cb2f44f02ba59f
Aaron Chavez <achavez@corporaciondpt.com>
2018-09-25 15:43:26 -0600

    finished lesson 23

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { formArrayNameProvider } from '../../../../node_modules/@angular/forms/src/directives/reactive_directives/form_group_name';
+import { controlNameBinding } from '../../../../node_modules/@angular/forms/src/directives/reactive_directives/form_control_name';
 
 @Component({
   selector: 'app-data',
@@ -24,7 +26,8 @@ export class DataComponent implements OnInit {
       name: 'Henry',
       lastName: 'Chaves'
     },
-    email: 'some@some.com'
+    email: 'some@some.com',
+    hobbies: ['Run','Sleep','Play']
   };
 
   constructor() { 
@@ -35,18 +38,66 @@ export class DataComponent implements OnInit {
 
       'fullName': new FormGroup({
         'name': new FormControl('', [Validators.required, Validators.minLength(3)] ),
-        'lastName': new FormControl('', Validators.required)
+        'lastName': new FormControl('', [Validators.required, this.nosomeLastName])
       }),
-      'email': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
-    })
+      'email': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      'hobbies': new FormArray([ new FormControl('Run', Validators.required )]),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl()
+      
+    });
 
+   //  this.forma.setValue( this.user );
 
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.passVal.bind(this.forma)
+    ])
 
   }
 
 
   saveChanges(){
     console.log(this.forma.value);
+     // this.forma.reset(this.user);
+   
+   this.forma.reset({
+    fullName:{
+      name: '',
+      lastName: ''
+    },
+    email: ''
+  });
+  }
+
+  addHobbies(){
+    (<FormArray>this.forma.controls['hobbies']).push(
+      new FormControl('', Validators.required)
+    )
+  }
+
+  nosomeLastName(control: FormControl): { [s:string]:boolean }  {
+
+    if( control.value === 'Chavez'){
+      return {
+        nosomelastname: true
+      }
+    }
+
+    return null;
+  
+  }
+
+  passVal(control: FormControl): { [s:string]:boolean }  {
+
+    if( control.value !== this.controls['password1'].value){
+      return {
+        passval: true
+      }
+    }
+
+    return null;
+  
   }
 
   ngOnInit() {
